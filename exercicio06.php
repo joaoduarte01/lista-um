67890c281b23536f7c4a80719588c1c093249bfb
joaoduarte01 <joao_duarte3@estudante.sesisenai.org.br>
2025-07-15 11:18:48 -0300

    exercicio6

---
+++ exercicio06.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" name="numero" id="numero" required>
+        <input type="submit" value="Verificar">
+    </form>
+    
+</body>
+</html>
