949f518c215081edfc2ff8f7471f594b6d6a4688
joaoduarte01 <joao_duarte3@estudante.sesisenai.org.br>
2025-07-15 12:33:51 -0300

    exercicio06

--- exercicio06.php
+++ exercicio06.php
@@ -1,16 +1,29 @@
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Divisores de um Número</title>
 </head>
 <body>
-    
-    <form method="POST" action="">
+
+    <form method="post" action="">
         <label for="numero">Digite um número:</label>
         <input type="number" name="numero" id="numero" required>
-        <input type="submit" value="Verificar">
+        <input type="submit" value="Mostrar Divisores">
     </form>
-    
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $numero = $_POST["numero"];
+
+        echo "<h3>Divisores de $numero:</h3>";
+
+        for($i = 1; $i <= $numero; $i++) {
+            if ($numero % $i == 0) {
+                echo "$i<br>";
+            }
+        }
+    }
+    ?>
+
 </body>
 </html>
